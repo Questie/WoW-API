6b4fe3ae2cf61eccfa78844decf45d2155216ac8
Logonz <Logon.q@gmail.com>
2025-12-09 18:29:53 +0100

    Removal of all injected functions

--- WoW-API/FrameXML/Blizzard_FrameXML/TransmogUtil.lua
+++ WoW-API/FrameXML/Blizzard_FrameXML/TransmogUtil.lua
@@ -3,12 +3,6 @@
 ---@class TransmogPendingInfoMixin
 TransmogPendingInfoMixin = {}
 
---- See [CreateAndInitFromMixin](https://www.townlong-yak.com/framexml/go/CreateAndInitFromMixin)
----@param pendingType Enum.TransmogPendingType
----@param transmogID number
----@param category number
-function TransmogPendingInfoMixin:Init(pendingType, transmogID, category) end
-
 ---@class TransmogLocationType
 ---@field slotID? InventorySlots
 ---@field type? Enum.TransmogType
@@ -18,41 +12,3 @@ function TransmogPendingInfoMixin:Init(pendingType, transmogID, category) end
 ---@class TransmogLocationMixin : TransmogLocationType
 TransmogLocationMixin = {}
 
----@param slotID number
----@param transmogType Enum.TransmogType
----@param modification Enum.TransmogModification
-function TransmogLocationMixin:Set(slotID, transmogType, modification) end
-
----@return boolean
-function TransmogLocationMixin:IsAppearance() end
-
----@return boolean
-function TransmogLocationMixin:IsIllusion() end
-
----@return number slotID
-function TransmogLocationMixin:GetSlotID() end
-
----@return string slotName
-function TransmogLocationMixin:GetSlotName() end
-
----@return boolean
-function TransmogLocationMixin:IsEitherHand() end
-
----@return boolean
-function TransmogLocationMixin:IsMainHand() end
-
----@return boolean
-function TransmogLocationMixin:IsOffHand() end
-
----@param transmogLocation TransmogLocationMixin
----@return boolean
-function TransmogLocationMixin:IsEqual(transmogLocation) end
-
----@return number armorCategoryID
-function TransmogLocationMixin:GetArmorCategoryID() end
-
----@return number lookupKey
-function TransmogLocationMixin:GetLookupKey() end
-
----@return boolean
-function TransmogLocationMixin:IsSecondary() end
--- WoW-API/FrameXML/Blizzard_NamePlates/Blizzard_NamePlates.lua
+++ WoW-API/FrameXML/Blizzard_NamePlates/Blizzard_NamePlates.lua
@@ -5,27 +5,11 @@
 ---@class NamePlateBaseMixin
 local NamePlateBaseMixin = {}
 
----@param namePlateUnitToken string
----@param driverFrame NamePlateDriverFrame
-function NamePlateBaseMixin:OnAdded(namePlateUnitToken, driverFrame) end
-
 function NamePlateBaseMixin:OnRemoved() end
 
 function NamePlateBaseMixin:OnOptionsUpdated() end
 
 function NamePlateBaseMixin:ApplyOffsets() end
-
----@param insetWidth number
----@param insetHeight number
----@return number widthPadding
----@return number heightPadding
-function NamePlateBaseMixin:GetAdditionalInsetPadding(insetWidth, insetHeight) end
-
----@return number left
----@return number right
----@return number top
----@return number bottom
-function NamePlateBaseMixin:GetPreferredInsets() end
 
 function NamePlateBaseMixin:OnSizeChanged() end
 
